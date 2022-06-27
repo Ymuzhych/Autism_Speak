12c6a31e36f18d4b1fbb678ee5a2cff9aa6473ab
Ymuzhych <myuliia2018@gmail.com>
2022-06-27 17:28:09 -0500

    fixed edit btn

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -1,6 +1,6 @@
 async function deleteFormHandler(event) {
   event.preventDefault();
-
+// get the post id from the url
   const post_id = window.location.toString().split("/")[
     window.location.toString().split("/").length - 1
   ];
@@ -8,7 +8,7 @@ async function deleteFormHandler(event) {
   const response = await fetch(`/api/posts/${post_id}`, {
     method: "DELETE",
   });
-
+// if the delete action is successful, redirect to the dashboard page, otherwise display the error
   if (response.ok) {
     document.location.replace("/dashboard");
   } else {
