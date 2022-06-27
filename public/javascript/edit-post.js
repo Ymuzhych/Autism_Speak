aa43bd21478b8650d4a7124a2b9e240ecbb78ee5
Ymuzhych <myuliia2018@gmail.com>
2022-06-26 22:01:14 -0500

    fixing edit btn

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,12 +1,23 @@
-async function deleteFormHandler(event) {
+async function editFormHandler(event) {
   event.preventDefault();
 
+  const title = document.querySelector('input[name="post-title"]').value;
+  const post_content = document
+    .querySelector('textarea[name="post-content"]')
+    .value.trim();
   const post_id = window.location.toString().split("/")[
     window.location.toString().split("/").length - 1
   ];
 
   const response = await fetch(`/api/posts/${post_id}`, {
-    method: "DELETE",
+    method: "PUT",
+    body: JSON.stringify({
+      title,
+      post_content,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
   });
 
   if (response.ok) {
@@ -17,5 +28,5 @@ async function deleteFormHandler(event) {
 }
 
 document
-  .querySelector(".delete-post-btn")
-  .addEventListener("click", deleteFormHandler);
+  .querySelector(".edit-post-form")
+  .addEventListener("submit", editFormHandler);
